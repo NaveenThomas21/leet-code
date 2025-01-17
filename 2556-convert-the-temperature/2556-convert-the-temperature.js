81484143e5541594d89e071c09ffd2d02196af5a
NaveenThomas21 <naveenthomas521@gmail.com>
2025-01-17 13:10:44 +0530

    Time: 0 ms (100%), Space: 49 MB (39.93%) - LeetHub

---
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function (celsius) {
+    let kelvin = celsius + 273.15;
+    let fahrenheit = celsius * 1.8 + 32;
+    return [kelvin, fahrenheit];
+};
+console.log(convertTemperature([36.50]))
+    
