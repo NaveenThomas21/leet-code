980e78af33eef8ae1b36dd2cae1b9219d3e54740
NaveenThomas21 <naveenthomas521@gmail.com>
2025-01-17 13:10:03 +0530

    Time: 0 ms (100%), Space: 48.5 MB (89.45%) - LeetHub

---
+++ 2383-add-two-integers/2383-add-two-integers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+
+    var sum = function(num1, num2) {
+   let a =Number(num1)
+   let b =Number(num2)
+   return a+b
+   };
+    
